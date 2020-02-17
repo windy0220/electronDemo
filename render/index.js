59548d538b5cb58eb06b771be0cd5977d58acd9f
马晨 <38280116@qq.com>
2020-02-17 21:23:26 +0800

    update:添加右键菜单

--- render/index.js
+++ render/index.js
@@ -1,28 +1,50 @@
 var fs = require('fs')
 const BrowserWindow = require('electron').remote.BrowserWindow //引入electron remote
-window.onload = function(){
+window.onload = function () {
     var btn = this.document.querySelector('#btn')
     var doit = this.document.querySelector('#doit')
     var open = this.document.querySelector('#open')
 
     // 读取文件
-    btn.onclick = function(){
-        fs.readFile('files/list.txt', (err, data)=>{
+    btn.onclick = function () {
+        fs.readFile('files/list.txt', (err, data) => {
             doit.innerHTML = data
         })
     }
 
     // 打开新窗口
-    open.onclick = function(){
+    open.onclick = function () {
         var newWindow = new BrowserWindow({
-            width:500,
-            height:300
+            width: 500,
+            height: 300
         })
 
         newWindow.loadFile('list.html')
-        newWindow.on('closed',()=>{
+        newWindow.on('closed', () => {
             newWindow = null
         })
 
     }
 }
+
+// 创建右键菜单
+const { remote } = require('electron')
+
+var rightMenuTemplate = [
+    {
+        label: '剪切',
+        accelerator: 'ctrl+x'
+    },
+    {
+        label: '粘贴',
+        accelerator: 'ctrl+v'
+    }
+]
+
+var rm = remote.Menu.buildFromTemplate(rightMenuTemplate)
+
+// 监听右键
+window.addEventListener('contextmenu', function(e){
+    e.preventDefault()
+    rm.popup({window:remote.getCurrentWindow()})
+})
