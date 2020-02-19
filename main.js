b36df424cbc09e93d03e274e67cad469d1e81667
马晨 <38280116@qq.com>
2020-02-19 23:02:28 +0800

    在窗口嵌入页面 BrowserView

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ app.on('ready', ()=>{
         webPreferences:{nodeIntegration:true} //全量启用node功能
     })
     // mainWindow.webContents.openDevTools() // 打开调试模式
+
+    //在窗口嵌入页面 BrowserView
+    var BrowserView = electron.BrowserView
+    var view = new BrowserView()
+    mainWindow.setBrowserView(view)
+    view.setBounds({x:0, y:120, width:800, height:680}) //设置嵌入的位置及宽高
+    view.webContents.loadURL('https://bigma.cc')
+
     require('./main/menu')
     mainWindow.loadFile('index.html')
     mainWindow.on('closed', ()=>{
